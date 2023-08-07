b4a7b43e346eedaecf5231150122cd010fa3a2b9
Marcos Brito <marcosvcbrito@gmail.com>
2023-08-07 14:42:16 -0300

    Initial Commit

---
+++ NotoriousTaskManagement/NotoriousTaskManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotoriousTaskManagementApp.swift
+//  NotoriousTaskManagement
+//
+//  Created by Marcos Vinicius Brito on 07/08/23.
+//
+
+import SwiftUI
+
+@main
+struct NotoriousTaskManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
